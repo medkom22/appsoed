2a769bcb0d6c99a23d1c86ce973c98b8795087a9
handika231 <76081296+handika231@users.noreply.github.com>
2021-12-27 10:50:38 +0700

    modifikasi main

--- lib/main.dart
+++ lib/main.dart
@@ -25,7 +25,7 @@ class MyApp extends StatelessWidget {
       ],
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
-        title: 'Appsoed Pro',
+        title: 'Appsoed Muantap Jiwa',
         routes: {
           DetailNews.routeName: (ctx) => const DetailNews(),
         },
